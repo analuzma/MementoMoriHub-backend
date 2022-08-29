3b2f9319e52fb9def7dc1514812810c415f3986d
ana luz martinez <almadigital.mx@gmail.com>
2022-08-29 12:39:18 -0700

    journal model done

---
+++ models/Journal.model.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const journalSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Journal entry must have a title"],
+    },
+    entry: {
+      type: String,
+      required: [true, "Journal entry must have content"],
+    },
+    //images2:[String], ["https://dylan.com/",....]
+    images: {
+      type: [
+        {
+          id: String,
+          url: String,
+          name: String,
+        },
+      ],
+      max: [3, "Only 3 images allowed per journal entry"],
+    },
+    date: {
+        type: Date,
+        default: Date.now
+       },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Journal", journalSchema);
