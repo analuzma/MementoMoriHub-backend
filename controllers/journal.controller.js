0717d0ef4408c204cd622698ad3dc90e0b7f694c
ana luz martinez <almadigital.mx@gmail.com>
2022-08-31 20:37:54 -0700

    Journal routes protected from other users :lock: and started favorite quotes feature

--- controllers/journal.controller.js
+++ controllers/journal.controller.js
@@ -1,9 +1,11 @@
 const Journal = require("../models/Journal.model");
+const favoriteQuote = require("../models/Favorites.model");
 const mongoose = require("mongoose");
 const { clearRes } = require("../utils/utils");
+const FavoriteQuote = require("../models/Favorites.model");
 
-//FALTA PROTEGER EL DETAIL Y EL DELETE
 
+//CREATE
 exports.createJournalEntry = async (req, res, next) => {
   const { title, entry, coverUrl, date, isFeatured } = req.body;
   const {_id:_author} = req.user
@@ -21,13 +23,13 @@ exports.createJournalEntry = async (req, res, next) => {
     return res.status(500).json({ errorMessage: error.message });
   }
 };
-
+//UPDATE
 exports.updateJournalEntry = async (req, res, next) => {
     const { id } = req.params;
     const {_id:_author} = req.user
   try {
-    const journalEntry = await Journal.findByIdAndUpdate(
-        { id, _author},
+    const journalEntry = await Journal.findOneAndUpdate(
+        { _id:id, _author},
         { ...req.body },
         { new: true }
     );
@@ -45,12 +47,12 @@ exports.updateJournalEntry = async (req, res, next) => {
     return res.status(500).json({ errorMessage: error.message });
   }
 };
-
+//DELETE
 exports.deleteJournalEntry = async (req, res, next) => {
   const { id } = req.params;
   const {_id:_author} = req.user
   try {
-    await Journal.findByIdAndDelete({id, _author});
+    await Journal.findOneAndDelete({_id:id, _author});
     res.status(201).json({ successMessage: "Journal entry has been deleted" });
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError)
@@ -62,7 +64,7 @@ exports.deleteJournalEntry = async (req, res, next) => {
     return res.status(500).json({ errorMessage: error.message });
   }
 };
-
+//GET ALL
 exports.getAllJournalEntries = async (req, res, next) => {
     const {_id:_author} = req.user
     try {
@@ -84,12 +86,12 @@ exports.getAllJournalEntries = async (req, res, next) => {
     return res.status(500).json({ errorMessage: error.message });
   }
 };
-
+//GET SINGLE
 exports.getJournalEntryById = async (req, res, next) => {
   const { id } = req.params;
   const {_id:_author} = req.user
   try {
-    const journalEntry = await Journal.findById({id},{_author});
+    const journalEntry = await Journal.findOne({_id:id, _author});
     const newJournalEntry = clearRes(journalEntry.toObject());
     res.status(201).json({ journalEntry: newJournalEntry });
   } catch (error) {
@@ -104,3 +106,61 @@ exports.getJournalEntryById = async (req, res, next) => {
     return res.status(500).json({ errorMessage: error.message });
   }
 };
+
+//DEMO FAVORITE
+
+exports.createFavorite = async (req,res,next) =>{
+  try{
+    //step1
+    const myList = await FavoriteQuote.findOne({_owner:req.user._id})
+    //verificamos si la lista exisita
+    if(myList){
+      //en caso de que si exista
+      const myListObject = myList.toObject()
+                  /////en lugar de find un replace para cuando quiera borrar
+      const favorite = myListObject.favoritesList.find(item=>
+        { console.log("verificar", item.id=== req.body.id)
+          return Number(item.id)===Number(req.body.id)})
+      console.log("favorite", favorite)
+      if(favorite){return res.status(201).json({successMessage:"chido pero ya lo tienes"})}
+      else {
+        //en caso de que no exista, lo agregamos
+        const updateFavorite = await FavoriteQuote.findOneAndUpdate({_owner:req.user._id},{$push:{favoritesList: {...req.body}}}, {new:true})
+        return res.status(201).json({successMessage:"creado", favorite:updateFavorite})}
+        //en caso de que no exista una lista de favoritos, la creamos
+      } else {
+      const favorite= await FavoriteQuote.create({favoritesList:[req.body], _owner:req.user._id})
+      res.status(201).json({ favorite });
+    }
+
+
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError)
+      return res.status(400).json({ errorMessage: error.message });
+
+    if (error.code === 11000)
+      return res
+        .status(400)
+        .json({ errorMessage: "Something happened while retrieving this journal" });
+
+    return res.status(500).json({ errorMessage: error.message });
+  }
+}
+
+exports.getFavorite = async (req,res,next) =>{
+  try{
+      const favorite= await FavoriteQuote.findOne({_owner:req.user._id})
+      res.status(201).json({ favorite});
+
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError)
+      return res.status(400).json({ errorMessage: error.message });
+
+    if (error.code === 11000)
+      return res
+        .status(400)
+        .json({ errorMessage: "Something happened while retrieving this journal" });
+
+    return res.status(500).json({ errorMessage: error.message });
+  }
+}
--- routes/journal.routes.js
+++ routes/journal.routes.js
@@ -5,19 +5,25 @@ const {
     deleteJournalEntry,
     getAllJournalEntries,
     getJournalEntryById,
+    createFavorite,
+    getFavorite
   } = require("../controllers/journal.controller");
 const { verifyToken, checkRole } = require("../middleware")
+//test favorite
 
+router.post("/createfav", verifyToken, createFavorite);
+
+router.get("/favlist", verifyToken, getFavorite);
 //Create
 router.post("/write", verifyToken, checkRole( ["User", "Admin"] ), createJournalEntry);
 //Read all entries list
 router.get("/", verifyToken, checkRole( ["User", "Admin"] ), getAllJournalEntries);
-//Read single entry
-router.get("/:id", verifyToken, checkRole( ["User", "Admin"] ), getJournalEntryById);
 //Update
 router.patch("/:id/edit", verifyToken, checkRole( ["User", "Admin"] ), updateJournalEntry);
 //Delete
 router.delete("/:id/delete", verifyToken, checkRole( ["User", "Admin"] ), deleteJournalEntry);
 
+//Read single entry
+router.get("/:id", verifyToken, checkRole( ["User", "Admin"] ), getJournalEntryById);
 
 module.exports = router;
